3e5d9693ac958f60e6ccbde3c2ecd45f19714c81
Jessica Lam <jemlam@jessicas-mbp.lan>
2020-02-26 19:06:38 -0800

    created global state in App.js and created weddingEventContext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route } from "react-router-dom";
 import './App.css';
 
+// Components
+import weddingEventContext from "./Contexts/WeddingEventContext";
+
 function App() {
+  const [weddingEvent, setweddingEvent] = useState([]);
   return (
     <div className='App'>
       <h1>Wedding Planner Portfolio</h1>
