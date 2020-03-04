c8670526035000c283342b04fdef8ce40d66d9dc
Brianna Workman <brianna.workman@hotmail.com>
2020-03-03 22:19:03 -0600

    Adds background image and styling to login and register forms

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,39 +1,55 @@
-import React, {useState} from 'react';
-import {axiosWithAuth} from './Authentication/axiosWithAuth'
+import React, { useState } from 'react';
+import { axiosWithAuth } from './Authentication/axiosWithAuth';
+import '../App.css';
 
 export default function Login(props) {
-
-const [user, getUser] = useState({
+  const [user, getUser] = useState({
     username: '',
     password: ''
-})
+  });
 
-const handleChanges = e => {
-    getUser({...user, [e.target.name]: e.target.value})
-}
+  const handleChanges = e => {
+    getUser({ ...user, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = event => {
     event.preventDefault();
     axiosWithAuth()
-    .post('/auth/login', user)
-    .then(res => {
-      console.log(res)
+      .post('/auth/login', user)
+      .then(res => {
+        console.log(res);
         localStorage.setItem('token', res.data.token);
         props.history.push('/protected');
         console.log('login form submitted');
-    })
-    .catch(err => {
+      })
+      .catch(err => {
         localStorage.removeItem('token');
         console.log('Invalid Login', err);
-    })
+      });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type='text' placeholder='username' name='username' onChange={handleChanges} value={user.username} />
-      <input type='text' placeholder='password' name='password' onChange={handleChanges} value={user.password} />
+    <div className='form-container'>
+      <form className='login-form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Username'
+          name='username'
+          onChange={handleChanges}
+          value={user.username}
+          className='field-container'
+        />
+        <input
+          type='text'
+          placeholder='Password'
+          name='password'
+          onChange={handleChanges}
+          value={user.password}
+          className='field-container'
+        />
 
-      <input type='submit' />
-    </form>
+        <input type='submit' className='button' />
+      </form>
+    </div>
   );
 }
