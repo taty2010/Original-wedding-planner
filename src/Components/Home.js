8083712707806c0572248b2a4f6dacc244b077a7
Brianna Workman <brianna.workman@hotmail.com>
2020-03-03 23:16:01 -0600

    adds styling to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import WeddingCard from './WeddingCard';
+import '../App.css';
 import weddingEventContext from '../Contexts/WeddingEventContext';
 
 export default function Home() {
@@ -17,18 +18,20 @@ export default function Home() {
   //       .catch(err => console.log(err.response));
   //   }
   return (
-    <div className='home'>
+    <div>
       <h1>Wedding Planner Portfolio</h1>
-      {weddingEvent.map(event => (
-        <WeddingCard
-          key={event.id}
-          image={event.image}
-          description={event.description}
-          location={event.location}
-          theme={event.theme}
-          vendors={event.vendors}
-        />
-      ))}
+      <div className='home'>
+        {weddingEvent.map(event => (
+          <WeddingCard
+            key={event.id}
+            image={event.image}
+            description={event.description}
+            location={event.location}
+            theme={event.theme}
+            vendors={event.vendors}
+          />
+        ))}
+      </div>
     </div>
   );
 }
